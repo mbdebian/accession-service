abf0640ec80ad8beab1aa9f979da6bb8b6fa74c1
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-18 19:51:55 +0100

    web service interface for the application, placeholder

---
+++ src/main/java/com/mbdebian/codingtasks/controllers/RestApi.java
@@ -0,0 +1,15 @@
+package com.mbdebian.codingtasks.controllers;
+
+/**
+ * @author Manuel Bernal Llinares
+ *         Project: accession-service
+ *         Package: com.mbdebian.codingtasks.controllers
+ *         Timestamp: 2017-07-18 19:51
+ *         ---
+ *         © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+ *         All rights reserved.
+ *
+ *         Web service interface for this application
+ */
+public class RestApi {
+}
