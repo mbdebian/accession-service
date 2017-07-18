189aded1a75ef56a311c6080f57ee020d72f3b03
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-18 19:49:14 +0100

    Main Application model

---
+++ src/main/java/com/mbdebian/codingtasks/models/AppModel.java
@@ -0,0 +1,15 @@
+package com.mbdebian.codingtasks.models;
+
+/**
+ * @author Manuel Bernal Llinares
+ *         Project: accession-service
+ *         Package: com.mbdebian.codingtasks.models
+ *         Timestamp: 2017-07-18 19:48
+ *         ---
+ *         © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+ *         All rights reserved.
+ *
+ *         This is the main application model (we won't need more for covering the requirements)
+ */
+public class AppModel {
+}
