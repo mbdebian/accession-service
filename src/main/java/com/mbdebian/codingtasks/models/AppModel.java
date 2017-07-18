39afc588ffe5c5b845fa92ae4c2606be157702bc
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-18 20:01:44 +0100

    builder pattern skeleton for collapsing consecutive IDs

--- src/main/java/com/mbdebian/codingtasks/models/AppModel.java
+++ src/main/java/com/mbdebian/codingtasks/models/AppModel.java
@@ -2,6 +2,7 @@ package com.mbdebian.codingtasks.models;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,9 +19,19 @@ import java.util.List;
 @Component
 public class AppModel {
 
-    // Helper class for collapsing consecutive accessions
+    // Helper class for collapsing consecutive accessions (Builder)
     class Collapser {
-        // TODO
+        private List<String> collapsing = new ArrayList<>();
+        private List<String> product = new ArrayList<>();
+
+        public void build(String accession) {
+            // TODO
+        }
+
+        public List<String> getProduct() {
+            // TODO
+            return product;
+        }
     }
 
     public List<String> sortAndAggregate(List<String> accessions) {
