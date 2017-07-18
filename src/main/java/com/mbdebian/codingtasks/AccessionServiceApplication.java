d25f7c98855824777b66b847c1b9cb314524e36b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-18 20:22:12 +0100

    the web service can also be used as a command line application

--- src/main/java/com/mbdebian/codingtasks/AccessionServiceApplication.java
+++ src/main/java/com/mbdebian/codingtasks/AccessionServiceApplication.java
@@ -2,14 +2,20 @@ package com.mbdebian.codingtasks;
 
 import com.mbdebian.codingtasks.models.AppModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccessionServiceApplication {
+public class AccessionServiceApplication implements CommandLineRunner {
 
 	@Autowired
 	private AppModel appModel;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		// TODO
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessionServiceApplication.class, args);
