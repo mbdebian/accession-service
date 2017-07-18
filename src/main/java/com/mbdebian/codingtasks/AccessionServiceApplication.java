b635f6a79159115323b9ed03b8334d3630d89d91
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-18 20:24:45 +0100

    differentiate between running as command line application or web service

--- src/main/java/com/mbdebian/codingtasks/AccessionServiceApplication.java
+++ src/main/java/com/mbdebian/codingtasks/AccessionServiceApplication.java
@@ -5,19 +5,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class AccessionServiceApplication implements CommandLineRunner {
 
-	@Autowired
-	private AppModel appModel;
+    @Autowired
+    private AppModel appModel;
 
-	@Override
-	public void run(String... strings) throws Exception {
-		// TODO
-	}
+    @Override
+    public void run(String... strings) throws Exception {
+        // TODO
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccessionServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // If we have command line args, we run as command line application, if not, as web service
+        if (args.length > 0) {
+            new SpringApplicationBuilder(AccessionServiceApplication.class)
+                    .web(false)
+                    .run(args);
+        } else {
+            // Show the logging subsystem again when running as web service
+            System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "web");
+            SpringApplication.run(AccessionServiceApplication.class, args);
+        }
+    }
 }
