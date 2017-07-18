d7c34e227ebec1f5057cadf4076931b7a57ebd2d
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-18 20:29:50 +0100

    unit test cases

--- src/test/java/com/mbdebian/codingtasks/models/AppModelTest.java
+++ src/test/java/com/mbdebian/codingtasks/models/AppModelTest.java
@@ -1,5 +1,10 @@
 package com.mbdebian.codingtasks.models;
 
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * @author Manuel Bernal Llinares
  *         Project: accession-service
@@ -20,5 +25,13 @@ public class AppModelTest {
     public AppModelTest(String givenInput, String expectedOutput) {
         this.givenInput = givenInput;
         this.expectedOutput = expectedOutput;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testCases() {
+        return Arrays.asList(new Object[][]{
+                {"A00000,A0001,ERR000111,ERR000112,ERR000113,ERR000115,ERR000116,ERR100114,ERR200000001,ERR200000002,ERR200000003,DRR2110012,SRR211001,ABCDEFG1",
+                        "A00000,A0001,ABCDEFG1,DRR2110012,ERR000111-ERR000113,ERR000115-ERR000116,ERR100114,ERR200000001-ERR200000003,SRR211001"}
+        });
     }
 }
