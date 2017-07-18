6fc048795145f93096ca7fb8a637940389724b61
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-18 20:21:35 +0100

    autowire the application main model to the application bootstrap

--- src/main/java/com/mbdebian/codingtasks/AccessionServiceApplication.java
+++ src/main/java/com/mbdebian/codingtasks/AccessionServiceApplication.java
@@ -1,10 +1,15 @@
 package com.mbdebian.codingtasks;
 
+import com.mbdebian.codingtasks.models.AppModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccessionServiceApplication {
+
+	@Autowired
+	private AppModel appModel;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessionServiceApplication.class, args);
